71fa7e59f6d88e6cf03c29406986501442dc7860
GersonST <gersonsevero90@gmail.com>
2021-06-16 23:57:47 -0300

    Exibir Issues

--- src/pages/Repositorio/index.js
+++ src/pages/Repositorio/index.js
@@ -1,25 +1,25 @@
 import React, { useState, useEffect } from 'react';
-import {Container, Owner, Loading, BackButton} from './styles';
+import { Container, Owner, Loading, BackButton, IssuesList } from './styles';
 import api from '../../services/api';
-import {FaArrowLeft} from 'react-icons/fa';
+import { FaArrowLeft } from 'react-icons/fa';
 //decodeURIComponent(match.params.repositorio)
 
-export default function Repositorio({match}){
+export default function Repositorio({ match }) {
 
 
   const [repositorio, setRepositorio] = useState({});
   const [issues, setIssues] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  useEffect(()=> {
+  useEffect(() => {
 
-    async function load(){
+    async function load() {
       const nomeRepo = decodeURIComponent(match.params.repositorio);
 
       const [repositorioData, issuesData] = await Promise.all([
         api.get(`/repos/${nomeRepo}`),
-        api.get(`/repos/${nomeRepo}/issues`,{
-          params:{
+        api.get(`/repos/${nomeRepo}/issues`, {
+          params: {
             state: 'open',
             per_page: 5
           }
@@ -36,35 +36,55 @@ export default function Repositorio({match}){
     load();
 
 
-  },[match.params.repositorio])
+  }, [match.params.repositorio])
 
 
-if(loading){
-  
-    return(
-<Loading>
-<h1>Carregando...</h1>
-</Loading>
+  if (loading) {
+
+    return (
+      <Loading>
+        <h1>Carregando...</h1>
+      </Loading>
     )
-  
-}
 
-  return(
+  }
+
+  return (
     <Container>
 
       <BackButton to="/">
-        <FaArrowLeft color="#000" size={35}/>
+        <FaArrowLeft color="#000" size={35} />
       </BackButton>
 
- 
+
 
       <Owner>
-        <img 
-        src={repositorio.owner.avatar_url} alt="avatar" 
+        <img
+          src={repositorio.owner.avatar_url} alt="avatar"
         />
         <h1>{repositorio.name}</h1>
         <p>{repositorio.description}</p>
       </Owner>
+
+      <IssuesList>
+        {issues.map(issue => (
+          <li key={String(issue.id)}>
+            <img src={issue.user.avatar_url} alt={issue.user.login} />
+
+            <div>
+              <strong>
+                <a href={issue.html_url}>{issue.title}</a>
+
+                {issue.labels.map(label => (
+                  <span key={String(label.id)}>{label.name}</span>
+                ))}
+              </strong>
+              <p>{issue.user.login}</p>
+            </div>
+
+          </li>
+        ))}
+      </IssuesList>
     </Container>
   );
 }
